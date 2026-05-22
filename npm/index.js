ebbdc4da374fb594b29a47101c50c351c9ad179c
Aadam Aftab <aadamaftab1126@gmail.com>
2026-05-22 14:56:31 +0530

    nodejs

---
+++ npm/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname";
+
+var sillyname = generateName();
+
+console.log(sillyname);
+
+import { randomSuperhero } from "superheroes";
+
+const heroName = randomSuperhero();
+
+console.log(heroName);
